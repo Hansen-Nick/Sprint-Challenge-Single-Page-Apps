c4acd7827e7e62a930b5f705d08b7cfd19bd6ec4
Nick <hansen.nick95@gmail.com>
2019-08-18 22:18:28 -0700

    Almost finished MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {Card, Image, Icon} from 'semantic-ui-react'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = (props) => {
+  return (
+    <Card> 
+      <Image src={props.image}/>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>{`${props.species} ${props.status}`}</Card.Meta>
+      <Card.Description><strong>Location:</strong> {props.location}<br></br><strong>Origin:</strong> {props.origin}</Card.Description>
+      <Card.Content extra>
+        <a>
+          <Icon name='user'></Icon>
+          Episodes: {props.episodes.length}
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
+
+export default CharacterCard
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Link } from "react-router-dom";
+import WelcomePage from './WelcomePage'
+import CharacterList from './CharacterList'
+import LocationsList from './LocationsList'
 
+
+
+const panes = [
+    {menuItem: 'Home', render: () => <Tab.Pane><Route exact path='/' component={WelcomePage} /></Tab.Pane>},
+    {menuItem: 'Characters', render: () => <Tab.Pane as={NavLink} to='/'><Route path='/characters' component={CharacterList} /></Tab.Pane>},
+    {menuItem: 'Locations', render: () => <Tab.Pane><Route path='/locations' component={LocationsList} /></Tab.Pane>}
+]
 // TODO: Add missing tabs below
-export default function TabNav() {
+const TabNav =() => <Tab panes={panes} />
 
-};
+export default TabNav
